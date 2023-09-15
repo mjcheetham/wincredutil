f0fc4d6c60b0ff55672e5be2da973bdfdfce4fea
Matthew John Cheetham <mjcheetham@outlook.com>
2023-09-15 11:59:23 -0700

    list: add show secret option

--- src/Program.cs
+++ src/Program.cs
@@ -8,23 +8,36 @@ var rootCmd = new RootCommand("Utility for interacting with the Windows Credenti
 var listCmd = new Command("list", "List credential entries.");
 var filterArg = new Option<string>("--filter", "Wildcard filter to match credentials.");
 var countArg = new Option<bool>("--count", "Print the number of returned entries only.");
+var showSecretArg = new Option<bool>("-w", "Print secret values as a string."); 
 listCmd.AddOption(filterArg);
 listCmd.AddOption(countArg);
-listCmd.SetHandler(List, filterArg, countArg);
+listCmd.AddOption(showSecretArg);
+listCmd.SetHandler(List, filterArg, countArg, showSecretArg);
 rootCmd.AddCommand(listCmd);
 
 return await rootCmd.InvokeAsync(args);
 
-void PrintCredential(Win32Credential c)
+void PrintCredential(Win32Credential c, bool showSecret)
 {
     Console.WriteLine("Target name   : {0}", c.TargetName);
     Console.WriteLine("User name     : {0}", c.UserName);
     Console.WriteLine("Type          : {0}", c.Type.ToString());
     Console.WriteLine("Last modified : {0:u}", c.LastWrittenDateTime);
+
+    if (showSecret)
+    {
+        string? secretStr = c.GetCredentialBlobAsString();
+        Console.WriteLine("Secret        : {0}", secretStr);
+    }
+    else
+    {
+        Console.WriteLine("Secret        : ********");
+    }
+
     Console.WriteLine();
 }
 
-void List(string? filter, bool countOnly)
+void List(string? filter, bool countOnly, bool showSecret)
 {
     var flags = string.IsNullOrWhiteSpace(filter)
         ? CredentialEnumerateFlags.AllCredentials
@@ -46,6 +59,6 @@ void List(string? filter, bool countOnly)
         IntPtr credPtr = Marshal.ReadIntPtr(ptr, i * ptrSize);
         Win32Credential credential = Marshal.PtrToStructure<Win32Credential>(credPtr);
 
-        PrintCredential(credential);
+        PrintCredential(credential, showSecret);
     }
 }
